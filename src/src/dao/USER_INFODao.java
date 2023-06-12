edd3790ba178b492d1610340fb30f7efb1389337
dojo2023-ishido <130334342+dojo2023-ishido@users.noreply.github.com>
2023-06-12 17:28:19 +0900

    DB更新

--- src/src/dao/USER_INFODao.java
+++ src/src/dao/USER_INFODao.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import model.USER_INFO;
+
 public class USER_INFODao {
 	//ログインできるならtrueを返す
 	public boolean isLoginOK(String id, String pw) {
@@ -59,7 +61,50 @@ public class USER_INFODao {
 		return loginResult;
 	}
 
-	//新規登録
+	// [アカウント新規登録]引数login_infoで指定されたレコードを登録し、成功したらtrueを返す
+	public boolean insert(USER_INFO login_info) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/src/data/gendaDB", "sa", "");
+
+			// SQL文を準備する(ID, PW, 社員番号, 秘密の質問, 秘密の質問の答え)
+			String sql = "insert into USER_INFO values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+
+			//SQL文を実行する
+			if(pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
 
 }
 
