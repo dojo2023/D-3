572000355d9fb1fca1a90ac4ae02cd1ad4aad26b
dojo2023-mezaki-r <130336148+dojo2023-mezaki-r@users.noreply.github.com>
2023-06-12 20:08:49 +0900

    ハッシュタグだお更新

--- src/src/dao/HASHTAGSDao.java
+++ src/src/dao/HASHTAGSDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -60,10 +61,10 @@ public class HASHTAGSDao {
 	}
 
 
-	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
-	public boolean insert(Bc card) {
-		Connection conn = null;
-		boolean result = false;
+	// ユーザーが入力した♯があるかの確認、探索
+	public boolean search(String hashtagname) {
+	    Connection conn = null;
+	    boolean result = false;
 
 		try {
 			// JDBCドライバを読み込む
@@ -73,112 +74,43 @@ public class HASHTAGSDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/src/data/gendaDB", "sa", "");
 
 			// SQL文を準備する
-			String sql = "insert into BC values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
+	        String sql = "SELECT COUNT(*) FROM HASHTAGS WHERE HASHTAGS_NAME = ?";
+	        PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			// SQL文を完成させる
-			if (card.getNumber() != null && !card.getNumber().equals("")) {
-				pStmt.setString(1, card.getNumber());
-			}
-			else {
-				pStmt.setString(1, null);
-			}
+	        // SQL文を完成させる
+	        if (hashtagname != null && !hashtagname.equals("")) {
+	            pStmt.setString(1, hashtagname);
+	        } else {
+	            pStmt.setString(1, null);
+	        }
 
-			if (card.getCompany() != null && !card.getCompany().equals("")) {
-				pStmt.setString(2, card.getCompany());
-			}
-			else {
-				pStmt.setString(2, null);
-			}
+	        // SQL文を実行し、結果を取得する
+	        ResultSet rs = pStmt.executeQuery();
 
-			if (card.getDepartment() != null && !card.getDepartment().equals("")) {
-				pStmt.setString(3, card.getDepartment());
-			}
-			else {
-				pStmt.setString(3, null);
-			}
+	     // 結果を確認する
+	        if (rs.next()) {
+	            int count = rs.getInt(1);
+	            if (count > 0) {
+	                result = true;
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } finally {
+	        // データベースを切断
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 
-			if (card.getPosition() != null && !card.getPosition().equals("")) {
-				pStmt.setString(4, card.getPosition());
-			}
-			else {
-				pStmt.setString(4, null);
-			}
-
-			if (card.getName() != null && !card.getName().equals("")) {
-				pStmt.setString(5, card.getName());
-			}
-			else {
-				pStmt.setString(5, null);
-			}
-
-			if (card.getZipcode() != null && !card.getZipcode().equals("")) {
-				pStmt.setString(6, card.getZipcode());
-			}
-			else {
-				pStmt.setString(6, null);
-			}
-
-			if (card.getAddress() != null && !card.getAddress().equals("")) {
-				pStmt.setString(7, card.getAddress());
-			}
-			else {
-				pStmt.setString(7, null);
-			}
-
-			if (card.getPhone() != null && !card.getPhone().equals("")) {
-				pStmt.setString(8, card.getPhone());
-			}
-			else {
-				pStmt.setString(8, null);
-			}
-
-			if (card.getFax() != null && !card.getFax().equals("")) {
-				pStmt.setString(9, card.getFax());
-			}
-			else {
-				pStmt.setString(9, null);
-			}
-
-			if (card.getEmail() != null && !card.getEmail().equals("")) {
-				pStmt.setString(10, card.getEmail());
-			}
-			else {
-				pStmt.setString(10, null);
-			}
-
-			if (card.getRemarks() != null && !card.getRemarks().equals("")) {
-				pStmt.setString(11, card.getRemarks());
-			}
-			else {
-				pStmt.setString(11, null);
-			}
-
-			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
+	    // 結果を返す
+	    return result;
 	}
 
 	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
