7ce6843badc91d8a0c3ba25936ae09e60eb1e7d0
dojo2023-furukawa <130334693+dojo2023-furukawa@users.noreply.github.com>
2023-06-13 13:44:03 +0900

    replyのモデル

--- src/src/model/REPLY.java
+++ src/src/model/REPLY.java
@@ -2,8 +2,8 @@ package model;
 import java.io.Serializable;
 
 public class REPLY implements Serializable {
-	private String REPLY_ID;
-	private String POSTER_ID;
+	private int REPLY_ID;
+	private int POSTER_ID;
 	private String REPLY_SENTENCE;
 	private String REPLIED_DATE;
 	private int USER_NAME_SWITCH;
@@ -11,7 +11,7 @@ public class REPLY implements Serializable {
 	private String ANIMAL_ID;
 
 
-	public  REPLY(String REPLY_ID, String POSTER_ID, String REPLY_SENTENCE, String REPLIED_DATE,
+	public  REPLY(int REPLY_ID, int POSTER_ID, String REPLY_SENTENCE, String REPLIED_DATE,
 			       int USER_NAME_SWITCH, String USER_ID, String ANIMAL_ID) {
 		super();
 		this.REPLY_ID = REPLY_ID;
@@ -25,24 +25,24 @@ public class REPLY implements Serializable {
 
 	public REPLY() {
 		super();
-		this.REPLY_ID = "";
-		this.POSTER_ID = "";
+		this.REPLY_ID = 0;
+		this.POSTER_ID = 0;
 		this.REPLY_SENTENCE = "";
 		this.REPLIED_DATE = "";
 		this.USER_NAME_SWITCH = 0;
 		this.USER_ID = "";
 		this.ANIMAL_ID = "";
 	}
-	public String getREPLY_ID() {
+	public int getREPLY_ID() {
 		return REPLY_ID;
 	}
-	public void setREPLY_ID(String REPLY_ID) {
+	public void setREPLY_ID(int REPLY_ID) {
 		this.REPLY_ID = REPLY_ID;
 	}
-	public String getPOSTER_ID() {
+	public int getPOSTER_ID() {
 		return POSTER_ID;
 	}
-	public void setPOSTER_ID(String POSTER_ID) {
+	public void setPOSTER_ID(int POSTER_ID) {
 		this.POSTER_ID = POSTER_ID;
 	}
 	public String getREPLY_SENTENCE() {
@@ -57,7 +57,7 @@ public class REPLY implements Serializable {
 	public void setREPLIED_DATE(String REPLIED_DATE) {
 		this.REPLIED_DATE = REPLIED_DATE;
 	}
-	public String getUSER_NAME_SWITCH() {
+	public int getUSER_NAME_SWITCH() {
 		return USER_NAME_SWITCH;
 	}
 	public void setUSER_NAME_SWITCH(int USER_NAME_SWITCH) {
