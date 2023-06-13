085263daad96d9b9dc66a1366835abd6a9608110
dojo2023-ogamino <130329974+dojo2023-ogamino@users.noreply.github.com>
2023-06-13 11:50:28 +0900

    ハッシュタグプログラム作成

--- src/src/dao/HASHTAGSDao.java
+++ src/src/dao/HASHTAGSDao.java
@@ -62,9 +62,9 @@ public class HASHTAGSDao {
 
 
 	// ユーザーが入力した♯があるかの確認、探索
-	public boolean search(String hashtagname) {
+	public String post_select(int hashtag_id) {
 	    Connection conn = null;
-	    boolean result = false;
+	    String hashtag_name = "";
 
 		try {
 			// JDBCドライバを読み込む
@@ -74,26 +74,20 @@ public class HASHTAGSDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/src/data/gendaDB", "sa", "");
 
 			// SQL文を準備する
-	        String sql = "SELECT COUNT(*) FROM HASHTAGS WHERE HASHTAGS_NAME = ?";
+	        String sql = "SELECT * FROM HASHTAGS WHERE HASHTAGS_ID = ?";
 	        PreparedStatement pStmt = conn.prepareStatement(sql);
 
 	        // SQL文を完成させる
-	        if (hashtagname != null && !hashtagname.equals("")) {
-	            pStmt.setString(1, hashtagname);
-	        } else {
-	            pStmt.setString(1, null);
-	        }
+	        pStmt.setInt(1, hashtag_id);
 
 	        // SQL文を実行し、結果を取得する
 	        ResultSet rs = pStmt.executeQuery();
 
-	     // 結果を確認する
-	        if (rs.next()) {
-	            int count = rs.getInt(1);
-	            if (count > 0) {
-	                result = true;
-	            }
-	        }
+	        // 結果を確認する
+	        rs.next();
+	        hashtag_name = rs.getString("HASHTAGS_NAME");
+
+
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    } catch (ClassNotFoundException e) {
@@ -110,12 +104,12 @@ public class HASHTAGSDao {
 	    }
 
 	    // 結果を返す
-	    return result;
+	    return hashtag_name;
 	}
 
 	// 掲示板に表示したらポスターデーターベースに接続して＃IDを持ってきて、その＃IDから＃の名前をもってくるのは、♯データーベースに接続。
 	//＃IDを入力したらそれに対応した関数を持ってくる必要がある
-	public boolean update(Bc card) {
+	public boolean search(String hashtag_name) {
 		Connection conn = null;
 		boolean result = false;
 
@@ -127,130 +121,19 @@ public class HASHTAGSDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
 
 			// SQL文を準備する
-			String sql = "update BC set COMPANY=?, DEPARTMENT=?, POSITION=?, NAME=?, ZIPCODE=?, ADDRESS=?, PHONE=?, FAX=?, EMAIL=?, REMARKS=? where NUMBER=?";
+			String sql = "SELECT COUNT (*) FROM HASHTAGS WHERE HASHTAGS_NAME LIKE ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			if (card.getCompany() != null && !card.getCompany().equals("")) {
-				pStmt.setString(1, card.getCompany());
-			}
-			else {
-				pStmt.setString(1, null);
-			}
+			pStmt.setString(1, hashtag_name);
 
-			if (card.getDepartment() != null && !card.getDepartment().equals("")) {
-				pStmt.setString(2, card.getDepartment());
-			}
-			else {
-				pStmt.setString(2, null);
-			}
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
 
-			if (card.getPosition() != null && !card.getPosition().equals("")) {
-				pStmt.setString(3, card.getPosition());
-			}
-			else {
-				pStmt.setString(3, null);
-			}
-
-			if (card.getName() != null && !card.getName().equals("")) {
-				pStmt.setString(4, card.getName());
-			}
-			else {
-				pStmt.setString(4, null);
-			}
-
-			if (card.getZipcode() != null && !card.getZipcode().equals("")) {
-				pStmt.setString(5, card.getZipcode());
-			}
-			else {
-				pStmt.setString(5, null);
-			}
-
-			if (card.getAddress() != null && !card.getAddress().equals("")) {
-				pStmt.setString(6, card.getAddress());
-			}
-			else {
-				pStmt.setString(6, null);
-			}
-
-			if (card.getPhone() != null && !card.getPhone().equals("")) {
-				pStmt.setString(7, card.getPhone());
-			}
-			else {
-				pStmt.setString(7, null);
-			}
-
-			if (card.getFax() != null && !card.getFax().equals("")) {
-				pStmt.setString(8, card.getFax());
-			}
-			else {
-				pStmt.setString(8, null);
-			}
-
-			if (card.getEmail() != null && !card.getEmail().equals("")) {
-				pStmt.setString(9, card.getEmail());
-			}
-			else {
-				pStmt.setString(9, null);
-			}
-
-			if (card.getRemarks() != null && !card.getRemarks().equals("")) {
-				pStmt.setString(10, card.getRemarks());
-			}
-			else {
-				pStmt.setString(10, null);
-			}
-
-			pStmt.setString(11, card.getNumber());
 
 			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
-	}
-
-	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
-	public boolean delete(String number) {
-		Connection conn = null;
-		boolean result = false;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
-			// SQL文を準備する
-			String sql = "delete from BC where NUMBER=?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			// SQL文を完成させる
-			pStmt.setString(1, number);
-
-			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
+			rs.next();
+			if (rs.getInt("COUNT(*)") == 1) {
 				result = true;
 			}
 		}
@@ -276,4 +159,3 @@ public class HASHTAGSDao {
 		return result;
 	}
 }
-
