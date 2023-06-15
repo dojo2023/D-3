8f5c231cd0aa2dbda25ee482163d568b62dbe1e9
dojo2023-mezaki-r <130336148+dojo2023-mezaki-r@users.noreply.github.com>
2023-06-15 17:25:06 +0900

    設定サーブレット更新

--- src/src/servlet/SettingServlet.java
+++ src/src/servlet/SettingServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -45,39 +44,77 @@ public class SettingServlet extends HttpServlet {
 
 		request.setCharacterEncoding("UTF-8");
 		String check = "NoChange";
+		HttpSession session = request.getSession();
+		String id = session.getId();
+		if(request.getParameter("password_change") != null) {//password_changeというパラメーターがリクエストに含まれているかのチェック
+			//含まれていれば、ユーザーがパスワードの変更を要求している
 
-		if(request.getParameter("password_change") != null) {
-			//ユーザのパスワードを変更する処理
-			String new_password = request.getParameter("password");
+			String new_password = request.getParameter("password");//ユーザのパスワードを変更する処理
+			//新しいパスワードを取得
+			//リクエストパラメーターからpasswordを取得して、new_passwordに格納
+
 			//このパスワードに変更する
-			HttpSession session = request.getSession();
-			String id = session.getId();
 
-			USER_INFO data = new USER_INFO();
-			data.setUser_id(id);
-
-			USER_INFODao search = new USER_INFODao();
-			List<USER_INFO> result = search.select(data);
-
-			USER_INFO aaa = result.get(0);
-			aaa.setUser_pw(new_password);
-			boolean updateresult = search.update(aaa);
+			USER_INFO data = new USER_INFO();//USER_INFOに新しいインスタンスdataを作成
+			data.setUser_id(id);//セッションIDを data オブジェクトのユーザIDとして設定
+			data.setUser_pw(new_password);
+			USER_INFODao search = new USER_INFODao();//USER_INFODaoに新しいインスタンスsearchを作成
+			boolean result = search.update(data);
 
 
 			}
 
 		else if(request.getParameter("news_change") != null) {
 			//ユーザの新着に表示するものを変更する処理
-			String new_item = request.getParameter("radio"); //これに"フリーワード"か"カテゴリー"か"タグ"が入ってる
-			String new_content = request.getParameter("new_content"); //これに登録する単語が入ってる
+			if (request.getParameter("radio").equals("フリーワード")){
+				String newfreeword = request.getParameter("news_content");
+				USER_INFO data1 = new USER_INFO();
+				data1.setUser_id(id);
+				data1.setFree_word(newfreeword);
+
+				USER_INFODao search = new USER_INFODao();
+				boolean result = search.update(data1);
+			}
+
+			else if (request.getParameter("radio").equals("カテゴリー")){
+				String newcategory = request.getParameter("news_content");
+				USER_INFO data2 = new USER_INFO();
+				data2.setUser_id(id);
+				data2.setFree_word(newcategory);
+
+				USER_INFODao search = new USER_INFODao();
+				boolean result = search.update(data2);
+			}
+
+			else if (request.getParameter("radio").equals("タグ")){
+				String newtag = request.getParameter("news_content");
+				USER_INFO data3 = new USER_INFO();
+				data3.setUser_id(id);
+				data3.setFree_word(newtag);
+
+				USER_INFODao search = new USER_INFODao();
+				boolean result = search.update(data3);
+			}
 		}
+
 		else if(request.getParameter("en_change") != null) {
 			//ユーザの社員番号を変更する処理
-			String new_en = request.getParameter("user_en"); //この社員番号に変更する
+
+			String new_en = request.getParameter("user_en");
+
+			//この社員番号に変更する
+
+			USER_INFO data4 = new USER_INFO();//USER_INFOに新しいインスタンスdataを作成
+			data4.setUser_id(id);//セッションIDを data オブジェクトのユーザIDとして設定
+			data4.setUser_en(new_en);
+			USER_INFODao search = new USER_INFODao();//USER_INFODaoに新しいインスタンスsearchを作成
+			boolean result = search.update(data4);
 		}
+
 		else if(request.getParameter("grant") != null) {
 			//ユーザの管理者権限を付与する処理
-			String user_id = request.getParameter("admin"); //このIDのユーザに管理者権限を付与する
+			String user_id = request.getParameter("admin");//このIDのユーザに管理者権限を付与する
+
 		}
 		else if(request.getParameter("revoke") != null) {
 			//ユーザの管理者権限を剥奪する処理
