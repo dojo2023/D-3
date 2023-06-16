e856b8a4b922be2fb38560b00876f245a0a3c647
dojo2023-kawaguchi-h <kawaguchiharukiplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-16 13:19:33 +0900

    Update PWResetServlet.java

--- src/src/servlet/PWResetServlet.java
+++ src/src/servlet/PWResetServlet.java
@@ -8,9 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.USER_INFODao;
-
+import model.USER_INFO;
 /**
  * Servlet implementation class IDServlet
  */
@@ -21,6 +22,7 @@ public class PWResetServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+
     public PWResetServlet() {
         super();
     }
@@ -37,22 +39,31 @@ public class PWResetServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	        // フォームからの入力値を取得
-	        String user_id = request.getParameter("user_id");
-	        String newPassword = request.getParameter("newPassword");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+	    String newPassword = request.getParameter("newPassword");
+	    String pwConfirm = request.getParameter("pwConfirm");
 
-	        // データベース更新の処理
-	        USER_INFODao userDao = new USER_INFODao();
-	        boolean success = userDao.updatePassword(user_id, newPassword);
-
-	        if (success) {
-	            // データベースの更新が成功した場合の処理
-	            response.getWriter().println("パスワードの更新に成功しました");
-	        } else {
-	            // データベースの更新が失敗した場合の処理
-	            response.getWriter().println("パスワードの更新に失敗しました");
-	        }
+	    // 新しいパスワードと確認用パスワードの一致を確認
+	    if (!newPassword.equals(pwConfirm)) {
+	        response.getWriter().println("パスワードが一致しません");
+	        return;
 	    }
 
+	    // データベース更新の処理
+	    USER_INFO pwReset = new USER_INFO();//USER_INFOに新しいインスタンスpwResetを作成
+	    pwReset.setUser_pw(newPassword);
+		USER_INFODao reset = new USER_INFODao();//USER_INFODaoに新しいインスタンスresetを作成
+	    boolean success =reset.update(pwReset);
+
+	    if (success) {
+	        // データベースの更新が成功した場合の処理
+	        response.getWriter().println("パスワードの更新に成功しました");
+	    } else {
+	        // データベースの更新が失敗した場合の処理
+	        response.getWriter().println("パスワードの更新に失敗しました");
+	    }
+	}
 }
