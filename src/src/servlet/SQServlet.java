b43a9a27c62f4e636ca99545ff2f6d87baf98357
dojo2023-ishido <130334342+dojo2023-ishido@users.noreply.github.com>
2023-06-19 10:44:51 +0900

    SQServlet　更新

--- src/src/servlet/SQServlet.java
+++ src/src/servlet/SQServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.USER_INFODao;
 import dao.USER_SQDao;
@@ -21,18 +22,19 @@ public class SQServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public SQServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/WebApp_GENDA/LoginServlet");
+			return;
+		}
 
+		// 秘密の質問ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WebApp_GENDA/jsp/sq.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -50,12 +52,12 @@ public class SQServlet extends HttpServlet {
 			USER_INFODao sqDao = new USER_INFODao();
 			USER_INFO user = sqDao.select(en, "");
 
-			 sq_id = user.getUser_sq_id();
+			sq_id = user.getUser_sq_id();
 		}else if(idf.equals("2")) {
 			USER_INFODao sqDao = new USER_INFODao();
 			USER_INFO user = sqDao.select("", id);
 
-			 sq_id = user.getUser_sq_id();
+			sq_id = user.getUser_sq_id();
 		}
 
 		USER_SQDao sq = new USER_SQDao();
@@ -65,9 +67,9 @@ public class SQServlet extends HttpServlet {
 		request.setAttribute("sq_name", sq_name); // sq_id をリクエストスコープに設定
 		request.setAttribute("idf", idf); // idf をリクエストスコープに設定
 
-	//結果ページにフォワードする
+		//結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/sq.jsp");
-	    dispatcher.forward(request, response);
-}
+		dispatcher.forward(request, response);
+	}
 }
 
